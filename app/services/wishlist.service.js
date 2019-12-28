956eea862ed19b30671a05bdeb47aa9791b06505
Ojo Tokunbo <ojo_tokunbo2002@yahoo.com>
2019-12-28 16:18:18 +0100

    added feature to follow an event

--- app/services/wishlist.service.js
+++ app/services/wishlist.service.js
@@ -13,5 +13,54 @@ module.exports = {
         query["deletedAt"] = null;
         let wishlist = await Wishlist.find(query);
         return wishlist;
-    }
+    },
+
+    /**
+     * creates a single wishlist instance
+     * @param wishlistData object
+     * 
+     */
+    createWishlist: async (wishlistData) => {
+
+        let wishlist = new Wishlist(wishlistData);
+        let result = await wishlist.save(); 
+        return result;
+    },
+
+
+    /**
+     * returns a single instance of a wishlist
+     * @param wishlistId String
+     */
+    viewWishlist: async (wishlistId) => {
+
+        let wishlist = await Wishlist.findById(wishlistId);
+        return wishlist;
+    },
+
+
+    /**
+     * update a single wishlist instance
+     * @param wishlistId integer
+     * @param updateData object
+     * 
+     */
+    updateWishlist: async (wishlistId, updateData) => {
+
+        const result = await Wishlist.findByIdAndUpdate( wishlistId, updateData, {new: true});
+        return result;
+    },
+
+
+    /**
+     * performs a softDelete operation on a single instance of a  model
+     * @param wishlistId integer
+     *
+     */
+    softDeleteWishlist: async (wishlistId) => {
+
+        console.log(wishlistId);
+        const updateData = { deletedAt: Date.now(), deletedBy: '1edfhuio3ifj' };
+        return await module.exports.updateWishlist(wishlistId, updateData); 
+    },
 }
