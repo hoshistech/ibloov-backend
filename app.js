1d4577b78d7ab2f799a03fd064a1bf477289b068
tokunboojo <tokunbo@esettlementgroup.com>
2020-05-13 20:00:24 +0100

    added support for cors.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 //moment
 //var moment = require('moment-timezone');
@@ -53,6 +54,9 @@ dbConnect.connect();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+//use cors
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
