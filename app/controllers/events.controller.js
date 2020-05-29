ccb475faf7ea99d0d707e154e850e269131c32e4
tokunboojo <tokunbo@esettlementgroup.com>
2020-05-29 01:54:18 +0100

    remove useragent log

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -30,10 +30,6 @@ module.exports = {
      */ 
     index: async (req, res) => {
 
-
-        console.log("req.useragent");
-        console.log(req.useragent);
-
         let filter = getMatch(req);
         let options = getOptions(req);
         filter["deletedAt"] = null; 
