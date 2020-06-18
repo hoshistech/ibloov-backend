d75be524986001a5010be512050635798450550f
tokunboojo <tokunbo@esettlementgroup.com>
2020-06-18 12:23:42 +0100

    ..

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -12,6 +12,6 @@ router.post('/checkout', [ checkAuth, validate("paymentCheckout"), isValidReques
 
 router.get('/braintree/generate/client_token', [ checkAuth ], PaymentController.generate_client_token);
 
-router.post('/stripe/token', [ checkAuth ], PaymentController.stipePaymentIntent);
+router.post('/stripe/token', [ checkAuth, validate("stripeToken"), isValidRequest ], PaymentController.stipePaymentIntent);
 
 module.exports = router;
